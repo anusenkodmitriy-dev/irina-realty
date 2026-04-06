62d906d09bbe92ae34066e063cb485a54e7367d1
Dmitry <anusenkodmitriy-dev@example.com>
2026-04-07 00:17:22 +0300

    Add PostgreSQL configuration and update static files settings

--- irina_realty/settings.py
+++ irina_realty/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -58,10 +59,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'irina_realty.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -82,7 +84,8 @@ STATICFILES_DIRS = [BASE_DIR / 'realty/static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
